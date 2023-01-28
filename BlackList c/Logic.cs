705aa0e46466af20c4cbea6815455f6e0518439d
Borovail <skorikdaniil17@gmail.com>
2023-01-28 18:24:42 +0200

    F

--- BlackList c/Logic.cs
+++ BlackList c/Logic.cs
@@ -12,25 +12,52 @@
     { 'h',    'w', ' ', 'w', ' ', 'w', ' ', 'w',' ' }
 };
 
+char[] tail = new char[6] { '#', ' ', 'w', 'b', 'W', 'B' };
 
-void MakeAMove(Point CurrentPosition,Point NextPosition)
+void MakeAMove(Point currentPosition,Point nextPosition,string WhosTurn)
 {
-  
+    char NextValue= Field[nextPosition.x, nextPosition.y];
+    if (NextValue.ToString() == tail[3].ToString().ToUpper())
+    {
+        /////абстрактная функция бить
+    }
 
-   
+    char CurrentValue = Field[currentPosition.x, currentPosition.y];
+    if (CurrentValue != ' ' && WhosTurn.ToLower()=="white" && CurrentValue!='b'&& NextValue.ToString()!= tail[3].ToString().ToUpper())
+    {
+    Field[nextPosition.x, nextPosition.y] = CurrentValue;
+    }
+    if (CurrentValue != ' ' && WhosTurn.ToString().ToLower() == "black" && CurrentValue != 'w'&& NextValue.ToString() != tail[4].ToString().ToUpper())
+    {
+        Field[nextPosition.x, nextPosition.y] = CurrentValue;
+    }
 
-
-    Field[posotion1, position] = 'D';
 }
 
 class Point
 {
-    public int x { get; set; }
-    private int y;
+    private int _x;
 
-    public int Y
+    public int x
     {
-        get { return y; }
+        get { return _x; }
+        set {
+            if (value > 0 && value < 9)
+            {
+                _x = value;
+            }
+            else
+            {
+                ////logic
+            }
+        }
+    }
+
+    private int _y;
+
+    public int y
+    {
+        get { return _y; }
         set {
             if (value == 'a') value = 1;
             if (value == 'b') value = 2;
@@ -39,7 +66,12 @@ class Point
             if (value == 'e') value = 5;
             if (value == 'f') value = 6;
             if (value == 'g') value = 7;
-            if (value == 'h') value = 8; y = value; }
+            if (value == 'h') value = 8; _y = value;
+             else
+            {
+                ////logic
+            }
+        }
     }
 
 }
